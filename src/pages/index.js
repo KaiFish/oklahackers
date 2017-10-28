d0037da51dd8b6d3e8fd612ca64ec5ae253f5b4c
Josh Birdwell <jbirdwell34@gmail.com>
2017-10-28 16:44:23 -0500

    Attempt at map projection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import Link from 'gatsby-link';
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+} from "react-simple-maps"
 
-const include = [
-  "MAR","DZA", "ZAF", "MUS", "TUN", "CPV", "ETH", "SYC", "NGA", "MDG",
-  "COD", "LBY", "GMB", "KEN", "GHA", "TZA", "MLI", "SDN", "SOM", "CIV",
-  "ZWE", "SEN", "CMR", "ERI", "UGA", "NAM", "REU", "MOZ", "AGO", "GAB",
-  "BFA", "RWA", "GIN", "TCD", "SSD", "MRT", "BWA", "NER", "DJI", "ZMB",
-  "SLE", "MWI", "LBR", "TGO", "BEN", "BDI", "SWZ", "COG", "LSO", "GNQ",
-  "STP", "EGY", "SOM", "CAF", "SOL", "ESH", "GNB"
-]
-
-const IndexPage = () => (
-  <div>
-    <h1>Oklahackers</h1>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
-
-export default IndexPage
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <ComposableMap>
+          <ZoomableGroup>
+          <Geographies geographyUrl={ "./oklahoma.json" }>
+            {(geographies, projection) => geographies.map((geography, i) => (
+              <Geography
+                key={ `geography-${i}` }
+                geography={ geography }
+                projection={ projection }
+                />
+            ))}
+          </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    )
+  }
+}
+export default App;
