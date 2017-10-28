7472dd8d2530468080af038d4325c2b771d102d5
Josh Birdwell <jbirdwell34@gmail.com>
2017-10-28 17:45:23 -0500

    stuf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,54 @@
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { geoMercator, geoPath } from "d3-geo";
 import {
   ComposableMap,
   ZoomableGroup,
   Geographies,
   Geography,
 } from "react-simple-maps"
+import Oklahoma from '../map/oklahoma.json';
 
 class App extends Component {
+  state = {
+    zoom: 50
+  }
+  
+  changeZoom = (change) => {
+    const { zoom } = this.state;
+    debugger;
+    this.setState({
+      zoom: zoom + change
+    });
+  }
+  
   render() {
+    const { zoom } = this.state;
+    
     return(
       <div>
-        <ComposableMap>
-          <ZoomableGroup>
-          <Geographies geographyUrl={ "./oklahoma.json" }>
-            {(geographies, projection) => geographies.map((geography, i) => (
-              <Geography
-                key={ `geography-${i}` }
-                geography={ geography }
-                projection={ projection }
-                />
-            ))}
+        <div onClick={() => this.changeZoom(-10)}>-</div>
+        <div onClick={() => this.changeZoom(10)}>+</div>
+        <ComposableMap
+          projectionConfig={{
+            scale: 160,
+          }}
+          projection="mercator"
+        >
+          <ZoomableGroup
+            center={[-97,35]}
+            zoom={zoom}
+          >
+          <Geographies geographyUrl='./oklahoma.json'>
+            {(geographies, projection) => geographies.map((geography, i) => {
+              return (
+                <Geography
+                  key={ `geography-${i}` }
+                  geography={ geography }
+                  projection={ projection }
+                  />
+              )
+            })}
           </Geographies>
           </ZoomableGroup>
         </ComposableMap>
